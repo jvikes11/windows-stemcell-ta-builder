499df9d0498c4406190e088e5dd4b52801a9a67b
Alan Moran <bonzofenix@gmail.com>
2019-06-06 16:07:06 +0000

    Adding code for upload and vm creation.
    Signed-off-by: Joe Goller <joe.goller@aon.com>

--- ci/scripts/deploy-base-image.sh
+++ ci/scripts/deploy-base-image.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
-echo "Deploy an ova"
+echo "Deploy iso for $OS_NAME"
+
+
+govc_iso_path="ISO/$OS_NAME.iso"
+
+
+govc datastore.upload \
+  -ds $GOVC_DATASTORE \
+  iso/os.iso \
+  $govc_iso_path
+
+govc create.vm \
+  -m $GOVC_MEMORY_MB \
+  -c $GOVC_NUM_CPU \
+  -disk $GOVC_DISK_GB \
+  -datastore-cluster $GOVC_CLUSTER \
+  -ds $GOVC_DATASTORE \
+  -g $GOVC_GUEST_OS \
+  -n $GOVC_NETWORK \
+  -iso-datastore $GOVC_DATASTORE \
+  -pool $GOVC_RESOURCE_POOL \
+  -host $GOVC_HOST \
+  -iso $govc_iso_path
 
 exit 1
