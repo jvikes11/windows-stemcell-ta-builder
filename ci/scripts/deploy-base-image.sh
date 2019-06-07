bf07304741673678402ef0f836c563d82772b6ac
Joe Goller <joe.goller@aon.com>
2019-06-07 00:07:15 +0000

    Adding floppy image to pipeline.

--- ci/scripts/deploy-base-image.sh
+++ ci/scripts/deploy-base-image.sh
@@ -10,25 +10,33 @@ echo "Deploy iso for $OS_NAME"
 
 govc_iso_path="ISO/$OS_NAME.iso"
 
+function upload_files() {
 
-govc datastore.upload \
-  -ds $GOVC_DATASTORE \
-  iso/os.iso \
-  $govc_iso_path
+  govc datastore.upload \
+    -ds $GOVC_DATASTORE \
+    iso/os.iso \
+    $govc_iso_path
 
-govc vm.create \
-  -m $GOVC_MEMORY_MB \
-  -c $GOVC_NUM_CPU \
-  -disk $GOVC_DISK_GB \
-  -dc $GOVC_DATACENTER \
-  -ds $GOVC_DATASTORE \
-  -folder $GOVC_FOLDER \
-  -g $GOVC_GUEST_OS \
-  -net $GOVC_NETWORK \
-  -iso-datastore $GOVC_DATASTORE \
-  -pool $GOVC_RESOURCE_POOL \
-  -host $GOVC_HOST \
-  -iso $govc_iso_path \
-  $GOVC_VM_NAME
+}
+
+function create_vm() {
+
+  govc vm.create \
+    -m $GOVC_MEMORY_MB \
+    -c $GOVC_NUM_CPU \
+    -disk $GOVC_DISK_GB \
+    -dc $GOVC_DATACENTER \
+    -ds $GOVC_DATASTORE \
+    -folder $GOVC_FOLDER \
+    -g $GOVC_GUEST_OS \
+    -net $GOVC_NETWORK \
+    -iso-datastore $GOVC_DATASTORE \
+    -pool $GOVC_RESOURCE_POOL \
+    -host $GOVC_HOST \
+    -iso $govc_iso_path \
+    -on false \
+    $GOVC_VM_NAME
+}
+
 
 exit 1
