eacad16563ccb271aeb505e320d87250a395bf9c
Joe Goller <joe.goller@aon.com>
2019-06-07 05:48:28 +0000

    Adjusting boot settings for mount.

--- ci/scripts/deploy-base-image.sh
+++ ci/scripts/deploy-base-image.sh
@@ -70,8 +70,7 @@ function initial_boot_vm() {
 #create_vm
 #initial_boot_vm
 
-govc vm.power -off $GOVC_VM_NAME
-govc vm.power -on $GOVC_VM_NAME
 govc vm.guest.tools -mount $GOVC_VM_NAME
+govc vm.power -on $GOVC_VM_NAME
 
 exit 1
