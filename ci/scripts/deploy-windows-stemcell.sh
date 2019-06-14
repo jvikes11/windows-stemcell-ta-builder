23178a8baa3b8d9ca2bcd6e120988a583e55603f
Alan Moran <bonzofenix@gmail.com>
2019-06-14 15:08:20 +0000

    Push built stemcell to artifactory via resource
    Signed-off-by: Joe Goller <joe.goller@aon.com>

--- ci/scripts/deploy-windows-stemcell.sh
+++ ci/scripts/deploy-windows-stemcell.sh
@@ -6,6 +6,7 @@ set -e
 source pipeline/ci/scripts/common.sh
 
 echo "Deploy stemcell for $OS_NAME"
+mkdir -p build
 
 stembuild_file_name=$(basename $(cat stembuild/metadata.json | jq -r '.ProductFiles | .[].AWSObjectKey' | grep linux))
 chmod +x stembuild/$stembuild_file_name
@@ -13,35 +14,34 @@ chmod +x stembuild/$stembuild_file_name
 cp lgpo-zip/LGPO.zip .
 
 function construct_stemcell() {
-
   echo "creating stemcell"
 
-  ./stembuild/$stembuild_file_name construct -vm-ip $VM_IP -vm-username $VM_ADMIN_USERNAME -vm-password $VM_ADMIN_PASSWORD -vcenter-url $GOVC_URL -vcenter-username $GOVC_USERNAME -vcenter-password $GOVC_PASSWORD -vm-inventory-path $GOVC_FOLDER/$GOVC_VM_NAME
+  ./stembuild/$stembuild_file_name construct \
+    -vm-ip $VM_IP \
+    -vm-username $VM_ADMIN_USERNAME \
+    -vm-password $VM_ADMIN_PASSWORD \
+    -vcenter-url $GOVC_URL \
+    -vcenter-username $GOVC_USERNAME \
+    -vcenter-password $GOVC_PASSWORD \
+    -vm-inventory-path $GOVC_FOLDER/$GOVC_VM_NAME
 
   govc vm.power -off -vm.ip $VM_IP
 
 }
 
 function package_stemcell() {
-
   echo "packaging stemcell"
 
-  ./stembuild/$stembuild_file_name package -vcenter-url $GOVC_URL -vcenter-username $GOVC_USERNAME -vcenter-password $GOVC_PASSWORD -vm-inventory-path $GOVC_FOLDER/$GOVC_VM_NAME
+  ./stembuild/$stembuild_file_name package \
+    -vcenter-url $GOVC_URL \
+    -vcenter-username $GOVC_USERNAME \
+    -vcenter-password $GOVC_PASSWORD \
+    -vm-inventory-path $GOVC_FOLDER/$GOVC_VM_NAME
 
-}
-
-function upload_stemcell() {
-
-  echo "uploading stemcell"
-
-  stemcell_tgz=$(ls bosh-stemcell*.tgz)
-
-  curl -u $ARTIFACTORY_USERNAME:$ARTIFACTORY_PASSWORD -i -k -X PUT -# -T $stemcell_tgz $ARTIFACTORY_URL/$stemcell_tgz
-
+  mv bosh-stemcell*.tgz build/.
 }
 
 construct_stemcell
 package_stemcell
-upload_stemcell
 
 exit 1
