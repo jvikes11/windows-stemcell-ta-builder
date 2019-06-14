c806c57be36a7a240c5a96b203a6a46439460d28
Alan Moran <bonzofenix@gmail.com>
2019-06-14 22:32:16 +0000

    Adding ip check
    Signed-off-by: Joe Goller <joe.goller@aon.com>

--- ci/scripts/deploy-base-image.sh
+++ ci/scripts/deploy-base-image.sh
@@ -66,16 +66,32 @@ function initial_boot_vm() {
 
   govc vm.power -on $GOVC_VM_NAME
 
+#  govc vm.info -json $GOVC_VM_NAME | jq -r '.VirtualMachines | .[].Summary.Runtime.PowerState'
+#
+  echo "waiting for machine to start"
+
+  while :; do
+    ping -q -c1 $VM_IP >/dev/null 2>&1
+    if [ $? -ne 0 ]; then
+      break
+    else
+      printf "\r."
+    fi
+    sleep 5
+  done
+
+  echo "machine started"
 }
 
 function customize_vm() {
   echo "customize vm"
 
+  pwsh config/default/windows_stemcell_builder/test.ps1
 }
 
-#upload_files
-#create_vm
-#initial_boot_vm
+upload_files
+create_vm
+initial_boot_vm
 customize_vm
 
 #govc vm.guest.tools -mount $GOVC_VM_NAME
