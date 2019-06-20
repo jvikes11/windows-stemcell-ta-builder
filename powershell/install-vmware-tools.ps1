a2c03b3dd5fa0564a1df62aacf33a9957572946a
Joe Goller <joe.goller@aon.com>
2019-06-20 00:44:25 +0000

    Fixed format errors
    Signed-off-by: Alan Moran <bonzofenix@gmail.com>

--- powershell/install-vmware-tools.ps1
+++ powershell/install-vmware-tools.ps1
@@ -3,7 +3,7 @@ $password = $env:VM_ADMIN_PASSWORD | ConvertTo-SecureString -AsPlainText -Force
 $creds = New-Object -TypeName System.Management.Automation.PSCredential $username,$password
 
 Invoke-Command -ComputerName $env:VM_IP -ScriptBlock {
-  $cd = Get-WMIObject -Class Win32_CDROMDrive
-  cd $cd.Drive
-  .\setup64.exe /s /v “/qn reboot=r”
+  $cd = Get-WMIObject -Class Win32_CDROMDrive;
+  cd $cd.Drive;
+  .\setup64.exe /s /v "/qn reboot=r";
 } -Authentication Negotiate -Credential $creds
